8113a2a1868cd5b5b9c1fa840a2f0b7ac410c0c2
Nikhil Chelani <nikhilchelani.94@gmail.com>
2022-07-10 13:55:13 +0400

    Added spacer & text components

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, SafeAreaView } from 'react-native';
+import { View, FlatList, SafeAreaView } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Searchbar } from 'react-native-paper';
 import styled from 'styled-components/native';
 
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
@@ -15,18 +16,25 @@ const SearchContainer = styled(View)`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled(View)`
-  flex: 1;
-  padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
 
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
     </SearchContainer>
-    <RestaurantListContainer>
-      <RestaurantInfoCard />
-    </RestaurantListContainer>
+    <RestaurantList
+      data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]}
+      renderItem={() => (
+        <Spacer size='large' position='bottom'>
+          <RestaurantInfoCard />
+        </Spacer>
+      )}
+      keyExtractor={(item) => item.name}
+    />
   </SafeArea>
 );
