ce6c374223c8d51f74347fd47a8fee6ec150d416
Nikhil Chelani <nikhilchelani.94@gmail.com>
2022-08-12 17:14:45 +0400

    Added account & login screens with firebase

---
+++ src/features/account/screens/account.screen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import {
+  AccountBackground,
+  AccountCover,
+  AccountContainer,
+  AuthButton,
+  Title,
+} from '../components/account.styles';
+import { Spacer } from '../../../components/spacer/spacer.component';
+
+export const AccountScreen = ({ navigation }) => {
+  return (
+    <AccountBackground>
+      <AccountCover />
+      <Title>Meals To Go</Title>
+      <AccountContainer>
+        <AuthButton
+          icon='lock-open-outline'
+          mode='contained'
+          onPress={() => navigation.navigate('Login')}
+        >
+          Login
+        </AuthButton>
+        <Spacer size='large'>
+          <AuthButton
+            icon='email'
+            mode='contained'
+            onPress={() => navigation.navigate('Register')}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
+    </AccountBackground>
+  );
+};
---
+++ src/services/authentication/authentication.context.js
@@ -0,0 +1,38 @@
+import React, { useState, createContext } from 'react';
+
+import { loginRequest } from './authentication.service';
+
+export const AuthenticationContext = createContext();
+
+export const AuthenticationContextProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+
+  const onLogin = (email, password) => {
+    setIsLoading(true);
+    loginRequest(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.toString());
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <AuthenticationContext.Provider
+      value={{
+        user,
+        isLoading,
+        error,
+        onLogin,
+        isAuthenticated: !!user,
+      }}
+    >
+      {children}
+    </AuthenticationContext.Provider>
+  );
+};
